d659a54bbe60a12b56eecd89d8d007099667418e
Ann Wolff <annwolffe@gmail.com>
2017-03-23 20:37:57 -0600

    Add cors

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const FoodsController = require('./lib/controllers/foods-controller.js')
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
+app.options('*', cors(corsOptions))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
